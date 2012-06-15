010068b9e57b2c6ac619f6fa7d22d33e5ddaa694
davidmus <davidmus@localhost>
2012-06-15 16:53:12 +0100

    Updating version number after tagged release.

--- NinjaTurtles/Properties/AssemblyInfo.cs
+++ NinjaTurtles/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyProduct("NinjaTurtles")]
 [assembly: AssemblyCopyright("Copyright © 2012 David Musgrove")]
 
-[assembly: AssemblyVersion("0.2.0.1")]
-[assembly: AssemblyFileVersion("0.2.0.1")]
+[assembly: AssemblyVersion("0.2.0.2")]
+[assembly: AssemblyFileVersion("0.2.0.2")]
 
 [assembly: InternalsVisibleTo("NinjaTurtles.Tests")]
