997dd4a8982767457d3d78483a8e257f3894f7c7
GordonBurgett <GordonBurgett@localhost>
2014-12-08 09:46:35 -0600

    Turtles now identify anonymous delegates and closures inside methods, and mutate the instructions inside them.

---
+++ NinjaTurtles/MutantMetaData.cs
@@ -0,0 +1,50 @@
+#region Copyright & licence
+
+// This file is part of NinjaTurtles.
+// 
+// NinjaTurtles is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// NinjaTurtles is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with NinjaTurtles.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Copyright (C) 2012-14 David Musgrove and others.
+
+#endregion
+
+using Mono.Cecil;
+
+namespace NinjaTurtles
+{
+    /// <summary>
+    /// A class containing metadata for a mutation test.
+    /// </summary>
+    public class MutantMetaData
+	{
+        /// <summary>
+        /// Gets or sets the description of the mutation test being run.
+        /// </summary>
+        public string Description { get; internal set; }
+
+		internal MethodDefinition MethodDefinition { get; set; }
+        internal int ILIndex { get; set; }
+        internal TestDirectory TestDirectory { get; set; }
+
+        /// <summary>
+        /// Gets the name of the target directory for the mutation test, to
+        /// which the test DLLs and mutated assembly have been copied.
+        /// </summary>
+        public string TestDirectoryName
+		{
+			get { return TestDirectory.FullName; }
+		}
+	}
+}
+
